348b20cbd054a13ede45c6caadd8abff11d8e8d5
andyjohn23 <andrewnick906@gmail.com>
2022-07-19 13:00:34 +0300

    completed assignment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :article_limit_reached
 
   def index
     articles = Article.all.includes(:user).order(created_at: :desc)
@@ -8,13 +9,24 @@ class ArticlesController < ApplicationController
 
   def show
     article = Article.find(params[:id])
-    render json: articles
+    cookies[:page_views] ||=0
+    cookies[:page_views] = cookies[:page_views].to_i + 1
+    session[:page_views] ||=0
+    session[:page_views] += 1
+    session[:page_view_remaaining] = 3
+    render json: article
   end
 
   private
 
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
+  end
+
+  def article_limit_reached
+    if session[:page_views] > 3
+      render json: {error: "Article viewing limit reached"}, status: :unauthorized
+    end
   end
 
 end
